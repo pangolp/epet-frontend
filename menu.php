7e9712205a1c92ac4f28a272c5b663ae549c8187
Pagani Walter <paganiwalter@gmail.com>
2019-05-07 10:51:48 -0300

    agregando estilos al menu

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
     <header id="header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container">
-            <a class="navbar-brand mx-auto" href="http://test.epet20.com.ar/"><i class="fas fa-graduation-cap"></i> Epet 20</a>
+            <a class="navbar-brand mx-auto text-white" href="http://test.epet20.com.ar/"><i class="fas fa-graduation-cap"></i> Epet 20</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon" id="span"></span>
             </button>
@@ -10,29 +10,29 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mx-auto">
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle text-white" href="#" id="navbarInstitucional" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Institucional
                   </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <div class="dropdown-menu" aria-labelledby="navbarInstitucional">
                     <a class="dropdown-item" href="institucional">Autoridades</a>
                     <a class="dropdown-item" href="#">Historia</a>
                     <a class="dropdown-item" href="#">Misión las autoridades</a>
                   </div>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="galeria">Galería</a>
+                  <a class="nav-link text-white" href="galeria">Galería</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="teoria">Teoria</a>
+                  <a class="nav-link text-white" href="teoria">Teoria</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="taller">Taller</a>
+                  <a class="nav-link text-white" href="taller">Taller</a>
                 </li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDocentes" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Docentes
                   </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <div class="dropdown-menu" aria-labelledby="navbarDocentes">
                     <a class="dropdown-item" href="docentes">Documentación</a>
                     <a class="dropdown-item" href="#">Capacitacion</a>
                     <a class="dropdown-item" href="#">Proyectos</a>
@@ -40,19 +40,19 @@
                   </div>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Biblioteca</a>
+                  <a class="nav-link text-white" href="#">Biblioteca</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Secretaria</a>
+                  <a class="nav-link text-white" href="#">Secretaria</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Asesoria</a>
+                  <a class="nav-link text-white" href="#">Asesoria</a>
                 </li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle text-white" href="#" id="navbarAlumnos" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Alumnos
                   </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <div class="dropdown-menu" aria-labelledby="navbarAlumnos">
                     <a class="dropdown-item" href="docentes">Documentación</a>
                     <a class="dropdown-item" href="#">Comunicados</a>
                     <a class="dropdown-item" href="#">Centro de estudiantes</a>
